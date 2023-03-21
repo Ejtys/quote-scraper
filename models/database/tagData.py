36de8600509553c28459dc2e7ffa3b92dcf6df4f
Ejtys <jwziebapn@gmail.com>
2023-03-21 23:36:08 +0100

    Add QuoteTagData class to help with make many to many relations between tags and quotes

--- models/database/tagData.py
+++ models/database/tagData.py
@@ -1,4 +1,7 @@
-from database import Database
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+from database.database import Database
 
 class TagData:
     TABLE_NAME= 'tags'
@@ -12,5 +15,20 @@ class TagData:
                                     (name,)):
             return False
         Database.unsafe_insert(TagData.TABLE_NAME, TagData.VALUE_NAMES, (name,))
+        return True
+    
+
+    
+class QuoteTagData:
+    TABLE_NAME= 'quotes_tags'
+    VALUE_NAMES =('quote_id', 'tag_id')
+
+    @classmethod
+    def insert(cls, quote_id:str, tag_id:str) -> bool:
+        #Checks if there is already such a entry.
+        if Database.query_fetch(f"SELECT * FROM {QuoteTagData.TABLE_NAME} WHERE quote_id = ? and tag_id = ?;",
+                                    (quote_id, tag_id)):
+            return False
+        Database.unsafe_insert(QuoteTagData.TABLE_NAME, QuoteTagData.VALUE_NAMES, (quote_id, tag_id))
         return True
     
