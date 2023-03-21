e578904a1b0b7dbcf97a46c16a0dd0140bf4cac6
Ejtys <jwziebapn@gmail.com>
2023-03-21 11:39:41 +0100

    Add location model

--- models/database/locationData.py
+++ models/database/locationData.py
@@ -1,4 +1,7 @@
-from database import Database
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+from database.database import Database
 
 class LocationData:
     TABLE_NAME = 'locations'
@@ -13,3 +16,13 @@ class LocationData:
             return False
         Database.unsafe_insert(LocationData.TABLE_NAME, LocationData.VALUE_NAMES, (city, country))
         return True
+
+    @classmethod
+    def get_unique(cls, city:str, country:str):
+        return Database.query_fetch(f"SELECT * FROM {LocationData.TABLE_NAME} WHERE city = ? and country = ?;",
+                                    (city, country))[0]
+    
+    @classmethod
+    def is_unique(cls, city:str, country:str):
+        return not Database.query_fetch(f"SELECT * FROM {LocationData.TABLE_NAME} WHERE city = ? and country = ?;",
+                                    (city, country))
---
+++ models/locationModel.py
@@ -0,0 +1,54 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+from database.database import Database
+from database.locationData import LocationData
+
+class Location:
+
+    def __init__(self, city:str, country:str, id:int =-1):
+        self.ID = id
+        self.city = city
+        self.country = country
+        if self.ID ==-1 and not LocationData.is_unique(city, country):
+            l = Location._get_unique(city, country)
+            self.ID = l.ID
+        elif self.ID == -1:
+            self.save()
+            l = Location._get_unique(city, country)
+            self.ID = l.ID
+
+    def save(self):
+        LocationData.insert(self.city, self.country)
+
+    def __repr__(self) -> str:
+        return f"<Location: {self.ID}, {self.city} in {self.country}>"
+
+    @classmethod
+    def from_tuple(cls, t:tuple):
+        return Location(t[1], t[2], t[0])
+    
+    @classmethod
+    def from_id(cls, id:int):
+        t = Database.get_by_unique_value(LocationData.TABLE_NAME, 'id', id)
+        return Location.from_tuple(t)
+    
+    @classmethod
+    def all(cls):
+        t = Database.fetch_all(LocationData.TABLE_NAME)
+        l =[]
+        for b in t:
+            l.append(Location.from_tuple(b))
+        return l
+    
+    @classmethod
+    def _get_unique(cls, city:str, country:str):
+        return Location.from_tuple(LocationData.get_unique(city, country))
+
+    
+for x in Location.all():
+    print(x)
+
+Location("Sosnowiec", 'Poland')
+Location("Gliwice", 'Poland')
+Location("Szczecin", 'Poland')
